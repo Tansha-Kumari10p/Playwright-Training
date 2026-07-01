539d266153864d78686a8a5a9539e24cf6a21a0f
Tansha-Kumari10p <tansha.kumari@10pearls.com>
2026-07-01 17:39:15 +0500

    Clarify storage state purpose in auth setup
    Added comment to clarify the purpose of storing login state.

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -3,7 +3,7 @@ import LoginPage from '../pages/login.js';
 
 const testdata = require("../Fixtures/constants.json");
 
-const authFile = 'playwright/.auth/authentication.json';  
+const authFile = 'playwright/.auth/authentication.json'; 
 
 setup('Log in to the application', async ({page}) => {
 
@@ -12,10 +12,8 @@ setup('Log in to the application', async ({page}) => {
     await loginPage.login(testdata.username, testdata.password);
     await expect(page).toHaveURL(`https://practicetestautomation.com/logged-in-successfully/`);
 
-    // Store the login state in the storage file for use in other test
-    await page.context().storageState({ path: authFile });
+    await page.context().storageState({ path: authFile }); //storing login state in storage file
 });
-
 
 
 
